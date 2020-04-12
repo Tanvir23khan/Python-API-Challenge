012ff27c02abe8b3879a78bf43fb6107f40d4088
Tanvir23khan <tanvir23.khan@gmail.com>
2020-04-11 20:14:55 -0700

    HW Submission

--- README.md
+++
@@ -1,2 +0,0 @@
-# Python-API-Challenge
-Analysis &amp; graphic visualization of the weather of 500+ cities across the world. 
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "b1154fb03e423380e5b8bc22afcd2082"
+
+# Google API Key
+g_key = "AIzaSyCPPYGjFgiXBdTU8-RGzbSyWZyrhZcEJRo"
